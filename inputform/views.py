292245d87839a84a65c194840f56fe12f2e587c4
Manasvi Trivedi <manasvitrivedi@gmail.com>
2021-06-10 23:06:06 +0530

    edit views.py

--- inputform/views.py
+++ inputform/views.py
@@ -19,23 +19,31 @@ def predict(request):
     lug_boot = request.POST['lug_boot']
     safety = request.POST['safety']
 
-    buying_dict = {'vhigh':3, 'high':0, 'med':2, 'low':1}
-    maint_dict = {'vhigh':3, 'high':0, 'med':2, 'low':1}
-    doors_dict = {'2':0, '3':1, '4':2, '5more':3}
-    persons_dict = {'2':0, '4':1, 'more':2}
-    lug_dict = {'small':2, 'med':1, 'big':0}
-    safety_dict = {'low':1, 'med':2, 'high':0}    
+    buying_dict = {'high':0, 'low':1, 'med':2, 'vhigh':3}
+    maint_dict = {'high':4, 'low':5, 'med':6, 'vhigh':7}
+    doors_dict = {'2':8, '3':9, '4':10, '5more':11}
+    persons_dict = {'2':12, '4':13, 'more':14}
+    lug_dict = {'big':15, 'med':16, 'small':17}
+    safety_dict = {'high':18, 'low':19, 'med':20}    
 
-    data = [buying_dict[buying], maint_dict[maint], doors_dict[doors], persons_dict[persons], lug_dict[lug_boot], safety_dict[safety]]
+    data = [0]*21
+    data[buying_dict[buying]] = 1
+    data[maint_dict[maint]] = 1
+    data[doors_dict[doors]] = 1
+    data[persons_dict[persons]] = 1
+    data[lug_dict[lug_boot]] = 1
+    data[safety_dict[safety]] = 1
+
     df = np.array(data)
     df = df.reshape(1,-1)
 
+    print(df)
+
     predictions = loaded_model.predict(df)
-    class_dict = {'2':'Unacceptable', '0':'Acceptable', '3':'Very good', '1':'Good'}
+    class_dict = {'unacc':'Unacceptable', 'acc':'Acceptable', 'vgood':'Very good', 'good':'Good'}
     output = class_dict[str(predictions[0])]
     print(output)
     msg = {'response': 'OK', 'class': output}
-    print(msg)
     return HttpResponse(json.dumps(msg), content_type='application/json')
 
 def index(request):
